961acef67e0104a20d95b6cde23957e71bc32492
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2020-12-17 16:24:04 +0200

    Release

---
+++ FlatAgency/FlatAgency/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Система управления квартирным агентством
+
+namespace FlatAgency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
